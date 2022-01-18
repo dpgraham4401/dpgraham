bf0f8e169bbf23f15cfbf46d56ed2c87de267fa0
David Paul Graham <dpgraham4401@gmail.com>
2022-01-17 21:00:23 -0500

    base article list functionality

--- main.go
+++ main.go
@@ -1,40 +1,96 @@
 package main
 
 import (
+	"errors"
 	// "fmt"
 	"html/template"
+	// "io/fs"
 	"log"
 	"net/http"
 	"os"
+	"regexp"
 )
 
 type Page struct {
 	Title string
 	Body  []byte
+    Path string
+    Articles []string
 }
 
-func (p *Page) save() error {
-	filename := p.Title + ".txt"
-	return os.WriteFile(filename, p.Body, 0600)
+func loadArticle(title string) (*Page, error) {
+    htmlDir := "./html/"
+	filename := htmlDir + title + ".txt"
+	body, _ := os.ReadFile(filename)
+    pageContent := Page{
+        Title: title,
+        Body: body,
+        Path: htmlDir,
+    }
+	return &pageContent, nil
 }
 
-func loadPage(title string) (*Page, error) {
-	filename := "html/" + title + ".txt"
-	body, err := os.ReadFile(filename)
-	if err != nil {
-		return nil, err
-	}
-	return &Page{Title: title, Body: body}, nil
+func (p *Page) readArticles() error {
+    dir := "./articles/"
+    f, _ := os.Open(dir)
+    files, _ := f.ReadDir(0)
+    for _, v := range files {
+        // fmt.Println(v.Name())
+        p.Articles = append(p.Articles, v.Name())
+    }
+    // p.Articles = ["test", "test2"]
+    return nil
+}
+
+var templatePaths = []string{
+    "./html/index.html",
+    "./html/list.html",
+}
+
+var templates = template.Must(template.ParseFiles(templatePaths...))
+
+func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
+    err := templates.ExecuteTemplate(w, tmpl + ".html", p)
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+    }
+}
+
+var validPath = regexp.MustCompile("^/(edit|save|view)/([a-zA-Z0-9]+)$")
+
+func getTitle(w http.ResponseWriter, r *http.Request) (string, error) {
+    m := validPath.FindStringSubmatch(r.URL.Path)
+    if m == nil {
+        http.NotFound(w, r)
+        return "", errors.New("invalid Page Title")
+    }
+    return m[2], nil
 }
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
-	p, _ := loadPage("home")
-	t, _ := template.ParseFiles("./html/home.html")
-	t.Execute(w, p)
-	// fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
+	p, _ := loadArticle("home")
+    renderTemplate(w, "index", p)
+}
+
+func blogHandler(w http.ResponseWriter, r *http.Request) {
+    if r.URL.Path == "/blog" {
+        // fmt.Println(len(r.URL.Path))
+        p, _ := loadArticle("home")
+        p.readArticles()
+        renderTemplate(w, "list", p)
+    } else {
+        p, _ := loadArticle("home")
+        renderTemplate(w, "list", p)
+    }
 }
 
 func main() {
-	http.HandleFunc("/", homeHandler)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+    fs := http.FileServer(http.Dir("assets"))
+
+	mux := http.NewServeMux()
+	mux.Handle("/assets/", http.StripPrefix("/assets/", fs))
+	mux.HandleFunc("/", homeHandler)
+	mux.HandleFunc("/blog", blogHandler)
+
+	log.Fatal(http.ListenAndServe(":8080", mux))
 }
